3f8beec60b4907c29a632d084700aeef83d38354
yosu <yosustri@gmail.com>
2020-07-30 11:14:46 +0000

    javadoc

--- webapp/src/main/java/com/gfi/Calculadora.java
+++ webapp/src/main/java/com/gfi/Calculadora.java
@@ -3,6 +3,10 @@ package com.gfi;
 
 public class Calculadora 
 {
+    /**
+     * Esta funcionm suma 2 numeritos
+     * @return El resultado de la suma.
+     * */
     public static int suma(int num1, int num2)
     {
         return num1+num2;
